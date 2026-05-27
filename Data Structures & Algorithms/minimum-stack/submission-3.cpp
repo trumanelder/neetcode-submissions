f03c22a4591bd7c68ff67aafc08db22801fee190
Truman Elder <82124462+trumanelder@users.noreply.github.com>
2026-05-27 14:05:58 -0700

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.cpp
@@ -0,0 +1,28 @@
+class MinStack {
+private:
+    vector<pair<int, int>> min_stack;
+    int minimum = INT_MAX;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(val < minimum) minimum = val;
+        min_stack.push_back(pair(minimum, val));
+    }
+    
+    void pop() {
+        min_stack.pop_back();
+        minimum = min_stack.size() ? get<0>(min_stack.back()) : INT_MAX;
+    }
+    
+    int top() {
+        return get<1>(min_stack.back());
+    }
+    
+    int getMin() {
+        return get<0>(min_stack.back());
+    }
+};
+ 
